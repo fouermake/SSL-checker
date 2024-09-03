8c6b8568e233c877b9a15b1791d7c63d40cb0978
FOUERMAKE <79097160+fouermake@users.noreply.github.com>
2024-09-03 16:44:23 +0300

    Update ssl_checker.py

--- ssl_checker.py
+++ ssl_checker.py
@@ -27,70 +27,38 @@ async def get_ssl_info(domain_or_ip):
         try:
             sock = await loop.run_in_executor(None, socket.create_connection, (domain, port))
         except (socket.gaierror, socket.error):
-            return {
-                'domain': domain_or_ip,
-                'type': 'Ошибка соединения',
-                'expiry_date': 'Не удалось установить подключение'
-            }
+            return {'domain': domain_or_ip, 'type': 'Ошибка соединения', 'expiry_date': 'Не удалось установить подключение'}
 
         try:
             ssock = context.wrap_socket(sock, server_hostname=domain)
             cert = ssock.getpeercert()
             
             if cert is None:
-                return {
-                    'domain': domain_or_ip,
-                    'type': 'Ошибка',
-                    'expiry_date': 'Сертификат не найден'
-                }
+                return {'domain': domain_or_ip, 'type': 'Ошибка', 'expiry_date': 'Сертификат не найден'}
 
             expiry_date = cert['notAfter']
             expiry_date_dt = datetime.strptime(expiry_date, '%b %d %H:%M:%S %Y GMT')
             expiry_date_str = expiry_date_dt.strftime('%d.%m.%Y')
 
             issuer = dict(x[0] for x in cert['issuer'])
-            cert_name = issuer.get('commonName', 'Сертификат истёк')
+            cert_name = issuer.get('commonName', 'Неизвестно')
 
             if expiry_date_dt < datetime.now():
-                return {
-                    'domain': domain_or_ip,
-                    'type': 'Ошибка SSL',
-                    'expiry_date': expiry_date_str
-                }
+                return {'domain': domain_or_ip, 'type': 'Сертификат истёк', 'expiry_date': expiry_date_str}
 
-            return {
-                'domain': domain_or_ip,
-                'type': cert_name,
-                'expiry_date': expiry_date_str
-            }
+            return {'domain': domain_or_ip, 'type': cert_name, 'expiry_date': expiry_date_str}
 
         except ssl.SSLError as ssl_error:
             error_message = str(ssl_error)
             if "certificate has expired" in error_message:
-                return {
-                    'domain': domain_or_ip,
-                    'type': 'Сертификат истёк',
-                    'expiry_date': expiry_date_str
-                }
+                return {'domain': domain_or_ip, 'type': 'Сертификат истёк', 'expiry_date': expiry_date_str}
             elif "self-signed certificate" in error_message:
-                return {
-                    'domain': domain_or_ip,
-                    'type': 'Cамоподписанный сертификат',
-                    'expiry_date': 'Неизвестно'
-                }
+                return {'domain': domain_or_ip, 'type': 'Самоподписанный сертификат', 'expiry_date': expiry_date_str}
             else:
-                return {
-                    'domain': domain_or_ip,
-                    'type': 'Ошибка SSL',
-                    'expiry_date': str(ssl_error)
-                }
+                return {'domain': domain_or_ip, 'type': 'Ошибка SSL', 'expiry_date': str(ssl_error)}
 
     except Exception as e:
-        return {
-            'domain': domain_or_ip,
-            'type': 'Ошибка',
-            'expiry_date': str(e)
-        }
+        return {'domain': domain_or_ip, 'type': 'Ошибка', 'expiry_date': str(e)}
 
 class SSLChecker:
     def __init__(self):
@@ -98,26 +66,22 @@ class SSLChecker:
         self.load_domains()
 
     def load_domains(self):
-        try:
-            script_dir = os.path.dirname(__file__)
-            json_file_path = os.path.join(script_dir, 'domain_list.json')
+        script_dir = os.path.dirname(__file__)
+        json_file_path = os.path.join(script_dir, 'domain_list.json')
 
-            with open(json_file_path, 'r', encoding='utf-8') as config_file:
-                config = json.load(config_file)
-                self.domains_list = config.get('domains', [])
-
-            print("\033[92m[INFO] Загружено доменов:", len(self.domains_list), "\033[0m")
-
-        except FileNotFoundError:
-            print("\033[93m[WARNING] Файл domain_list.json не найден.\033[0m")
-        except json.JSONDecodeError:
-            print("\033[91m[ERROR] Ошибка чтения JSON. Проверьте формат файла.\033[0m")
+        if os.path.exists(json_file_path):
+            try:
+                with open(json_file_path, 'r', encoding='utf-8') as config_file:
+                    config = json.load(config_file)
+                    self.domains_list = config.get('domains', [])
+                print(f"\033[92m[INFO] Загружено доменов: {len(self.domains_list)}\033[0m")
+            except json.JSONDecodeError:
+                print("\033[91m[ERROR] Ошибка чтения JSON. Проверьте формат файла.\033[0m")
+        else:
+            print("\033[93m[WARNING] Файл domain_list.json не найден. Начните с добавления доменов.\033[0m")
 
     async def save_domains(self):
-        domain_data = {
-            "domains": self.domains_list
-        }
-
+        domain_data = {"domains": self.domains_list}
         json_file_path = os.path.join(os.path.dirname(__file__), 'domain_list.json')
 
         async with aiofiles.open(json_file_path, 'w', encoding='utf-8') as config_file:
@@ -138,9 +102,7 @@ class SSLChecker:
             
         print("\033[93m[INFO] Начинается проверка SSL сертификатов. Пожалуйста, подождите...\033[0m")
         
-        results = await asyncio.gather(
-            *(get_ssl_info(domain) for domain in self.domains_list)
-        )
+        results = await asyncio.gather(*(get_ssl_info(domain) for domain in self.domains_list))
 
         print("\n\033[92m{: <30} {: <50} {: <10}\033[0m".format("Домен", "Тип сертификата", "Дата окончания"))
         print("-" * 100)
@@ -184,9 +146,9 @@ class SSLChecker:
 
             self.domains_list.append(domain)
             added_domains.append(domain)
-        
+
         await self.save_domains()
-        
+
         if added_domains:
             print(f"\033[92m[INFO] Добавлены домены: {', '.join(added_domains)}.\033[0m")
 
@@ -218,7 +180,7 @@ class SSLChecker:
             "author": "fouermake",
             "creation_date": "09.03.2024"
         }
-        
+
         print("\n\033[92m{: <50} {: <20} {: <25}\033[0m".format("Домен", "Автор", "Дата создания программы"))
         print("-" * 100)
         print("{: <50} {: <20} {: <25}".format(github_info['domain'], github_info['author'], github_info['creation_date']))
@@ -237,14 +199,14 @@ async def main():
                                                                                   
 Доступные команды:
 \033[93m1. list (ll, ls)\033[0m - показать список добавленных доменов.
-\033[93m2. add <домен1, домен2, ...> \033[0m - добавить домены в список (через запятую).
-\033[93m3. remove (rm) <домен1, домен2, ...> \033[0m - удалить домены из списка (через запятую).
-\033[93m4. check ssl \033[0m - проверить SSL сертификаты добавленных доменов.
-\033[93m6. exit \033[0m - выход из программы.
+\033[93m2. add <домен1, домен2, ...>\033[0m - добавить домены в список (через запятую).
+\033[93m3. remove (rm) <домен1, домен2, ...>\033[0m - удалить домены из списка (через запятую).
+\033[93m4. check ssl\033[0m - проверить SSL сертификаты добавленных доменов.
+\033[93m6. exit\033[0m - выход из программы.
 """
 
     print(ssl_icon)
-    
+
     while True:
         command = input("\033[92m$ \033[0m").strip().lower()
 
